2c2faafcee4d631eec2f33e5eb27f5426836a5d4
pratyush <pratyush.desai333@gmail.com>
2021-11-28 20:47:18 +0530

    proj added

---
+++ dms_miniproject/src/dms_miniproject/Dms_miniproject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dms_miniproject;
+
+/**
+ *
+ * @author praty
+ */
+public class Dms_miniproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        home h = new home();
+        h.setVisible(true);
+    }
+    
+}
